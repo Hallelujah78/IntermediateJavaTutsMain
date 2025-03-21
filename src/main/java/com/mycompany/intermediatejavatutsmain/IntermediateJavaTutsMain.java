3e6874c23062ad3a42ac6a9439aca318c6b37619
Gavan Browne <gavan.browne@gmail.com>
2025-03-21 11:39:52 +0000

    Initial commit

---
+++ src/main/java/com/mycompany/intermediatejavatutsmain/IntermediateJavaTutsMain.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.intermediatejavatutsmain;
+
+/**
+ *
+ * @author gavan
+ */
+public class IntermediateJavaTutsMain {
+
+}
---
+++ src/main/java/com/mycompany/intermediatejavatutsmain/Tut12.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.intermediatejavatutsmain;
+
+import java.util.*;
+
+/**
+ *
+ * @author gavan
+ */
+public class Tut12 {
+
+    public static void main(String[] args) {
+
+        // String array.
+        String[] strArr = {"apple", "oranges", "corn", "ham"};
+
+        // Convert to list.
+        List<String> strList = Arrays.asList(strArr);
+
+    }
+
+}
