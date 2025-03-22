aa2aa29803df93a867dec8df8c5fcb455b517d76
Gavan Browne <gavan.browne@gmail.com>
2025-03-22 13:38:13 +0000

    Update Tut14
    Explore push and pop from Stack.

--- src/main/java/com/mycompany/intermediatejavatutsmain/Tut14.java
+++ src/main/java/com/mycompany/intermediatejavatutsmain/Tut14.java
@@ -5,10 +5,47 @@
 package com.mycompany.intermediatejavatutsmain;
 
 // https://www.youtube.com/watch?v=8TMBjfS8wY0
+import java.util.Stack;
+
 /**
  *
  * @author gavan
  */
 public class Tut14 {
+// The stack - LIFO
+
+    private static void printStack(Stack<String> stack) {
+        for (String string : stack) {
+            System.out.printf("%s ", string);
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+
+        Stack<String> strStack = new Stack();
+
+        strStack.push(
+                "first");
+        printStack(strStack);
+        strStack.push(
+                "second");
+        printStack(strStack);
+
+        strStack.push(
+                "third");
+        printStack(strStack);
+
+        strStack.pop(); // we pop "third"
+        printStack(strStack);
+
+        strStack.pop(); // we pop "second"
+        printStack(strStack);
+
+        strStack.pop(); // we pop "first"
+        printStack(strStack); // stack is empty
+
+//        strStack.pop(); // Will cause EmptyStackException, must check if empty.
+    }
 
 }
