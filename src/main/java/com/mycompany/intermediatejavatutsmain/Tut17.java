e01b9d7862b7b08ae9277b8503943682764edee6
Gavan Browne <gavan.browne@gmail.com>
2025-03-24 14:08:54 +0000

    Finish tutorial 17
    We created printMe for Integer[] and overloaded it so it could take Character[]. Next we'll look at generics.

--- src/main/java/com/mycompany/intermediatejavatutsmain/Tut17.java
+++ src/main/java/com/mycompany/intermediatejavatutsmain/Tut17.java
@@ -11,4 +11,25 @@ package com.mycompany.intermediatejavatutsmain;
 // https://www.youtube.com/watch?v=J6B_qauxfuc&list=PL27BCE863B6A864E3&index=17
 public class Tut17 {
 
+    private static void printMe(Integer[] i) {
+        for (Integer item : i) {
+            System.out.printf("%s ", item);
+        }
+    }
+
+    private static void printMe(Character[] c) {
+        for (Character item : c) {
+            System.out.printf("%s ", item);
+        }
+    }
+
+    // Generic classes and methods - can eliminate overloaded methods with different types
+    public static void main(String[] args) {
+        Integer[] iArr = {1, 2, 3, 4};
+        Character[] cArr = {'a', 'b', 'c', 'd'};
+
+        printMe(iArr);
+        printMe(cArr);
+
+    }
 }
